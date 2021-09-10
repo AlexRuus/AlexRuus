2a32ac8f1d955e6543e6a1c32d104806d3e61136
AlexRuus <90018991+AlexRuus@users.noreply.github.com>
2021-09-10 07:42:03 +0300

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,13 @@
+
+
+function randomBackground() {
+
+    let arr = []
+    for(i = 0; i < 3; i++){
+    arr[i] = Math.floor(Math.random() * 256);
+    }
+
+ let backgroundR = "RGB(" + arr + ")";
+ document.body.style.backgroundColor = backgroundR;
+ document.querySelector('h1').innerHTML = backgroundR
+}
